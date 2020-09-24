4fb52a7c89fb029d47fbc37972859e813c3f2322
csm-kr <csm8167@naver.com>
2020-09-24 11:08:56 +0900

    sobolev norm grad cam beta version

--- grad_cam_module_1.py
+++ grad_cam_module_1.py
@@ -26,63 +26,17 @@ class Grad_Cam(nn.Module):
                         module.register_forward_hook(self.forward_hook)
                         module.register_backward_hook(self.backward_hook)
 
-    def forward(self, input, target_index, img):
-
-        outs = self.model(input).squeeze()
-
+    def forward(self, input, target_index):
         # ---------------------------------- for one targets ----------------------------------
-        # backward를 통해서 자동으로 backward_hook 함수가 호출되고, self.backward_result에 gradient가 저장됩니다.
+        outs = self.model(input)
+        outs = outs.squeeze()  # [1, 20]  --> [20]
         outs[target_index].backward(retain_graph=True)
-
-        self.backward_result = F.relu(self.backward_result)
-        # gradient의 평균을 구합니다. (alpha_k^c)
-        #a_k = torch.mean(self.backward_result, dim=(1, 2), keepdim=True)
-        #a_k = torch.mean(self.backward_result.pow(1), dim=(1, 2), keepdim=True).pow(1)
-
-        #b_k, _ = self.backward_result.view(512, -1).min(dim=1) - candidate 1 + ReLU
-
-        b_k, _ = torch.topk(-self.backward_result.view(512, -1), 49, dim=1)
-        b_k, _ = torch.topk(torch.log( self.backward_result.view(512, -1) ).pow(-1), 49, dim=1)
-        #a_k = torch.sum((-b_k[:, 0:45]).pow(3), dim=1 ).pow(1/3).view(-1, 1, 1)
-
-        b_k = -1*b_k[:, 0].view(-1, 1, 1)
-
-        #b_k = torch.mean(self.backward_result.pow(5), dim=(1, 2), keepdim=True).pow(1/5)
-        d_k = torch.sum(self.backward_result, dim=(1, 2), keepdim=True)
-
-
-        # self.foward_result를 이용하여 Grad-CAM을 계산합니다.
-        c_k =  b_k
-        #c_k = d_k
-        out = torch.sum(c_k * self.forward_result, dim=0).cpu()
-        # normalize
-        out = (out + torch.abs(out)) / 2
-        out = out / torch.max(out)
+        a_k = torch.mean(self.backward_result, dim=(1, 2), keepdim=True)
+        out = torch.sum(a_k * self.forward_result, dim=0).cpu()
+        out = torch.relu(out) / torch.max(out)  # 음수를 없애고, 0 ~ 1 로 scaling
         out = F.upsample_bilinear(out.unsqueeze(0).unsqueeze(0), [224, 224])  # 4D로 바꿈
-        show_cam_on_image(img, out.detach().squeeze().numpy())
         # ---------------------------------- for one targets ----------------------------------
-
-        # ---------------------------------- for batch targets ----------------------------------
-        # targets_mask = (outs - outs.min()) / (outs.max() - outs.min()) > 0.5
-        # sum_labels = torch.zeros([outs.size(0), 7, 7]).cuda()
-        # for batch, target_mask in enumerate(targets_mask):  # batch 를 돌면서
-        #     indices = torch.LongTensor(range(outs.size(1)))
-        #     for idx in indices[target_mask]:
-        #         outs[batch, idx].backward(retain_graph=True)
-        #         # mean of gradient : (alpha_k^c)
-        #         a_k = torch.mean(self.backward_result[batch], dim=(1, 2), keepdim=True)
-        #         out = torch.sum(a_k * self.forward_result[batch], dim=0).cuda()
-        #         # normalize
-        #         out = (out + torch.abs(out)) / 2
-        #         sum_labels[batch] += out
-        #         print(self.label[idx.item()])
-        #
-        #     out = sum_labels[batch] / torch.max(sum_labels)
-        #     out = F.upsample_bilinear(out.unsqueeze(0).unsqueeze(0), [224, 224])  # 4D로 바꿈
-        #     # print(t_idx)
-        #     show_cam_on_image(img, out.cpu().detach().squeeze().numpy())
-
-        return 0
+        return out.cpu().detach().squeeze().numpy()
 
     def forward_hook(self, _, input, output):
         self.forward_result = torch.squeeze(output)
@@ -150,8 +104,7 @@ if __name__ == '__main__':
         img = cv2.imread(img_path, 1)
         img = np.float32(cv2.resize(img, (224, 224))) / 255
         input = preprocess_image(img)
-        # input = torch.cat([input, input], dim=0).cuda()
-        # input = torch.randn(2, 3, 224, 224)
+
 
         # voc labels
         '''
@@ -160,7 +113,6 @@ if __name__ == '__main__':
          'diningtable', 'dog', 'horse', 'motorbike', 'person',
          'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor')
         '''
-
         target_index = 11
-        grad_cam(input, target_index, img)
-        # show_cam_on_image(img, mask)
+        mask = grad_cam(input, target_index)
+        show_cam_on_image(img, mask)
